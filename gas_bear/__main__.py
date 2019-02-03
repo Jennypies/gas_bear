131b42d29ab2db669a873777269fff2e80c0f364
Jenny <jenny.thorne3006@gmail.com>
2019-02-03 16:51:10 +0000

    pushy

--- gas_bear/__main__.py
+++ gas_bear/__main__.py
@@ -16,10 +16,10 @@ def main():
     sensor.set_temperature_oversample(bme680.OS_8X)
     sensor.set_filter(bme680.FILTER_SIZE_3)
 
-    sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
-    sensor.set_gas_heater_temperature(320)
-    sensor.set_gas_heater_duration(150)
-    sensor.select_gas_heater_profile(0)
+    #sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+    #sensor.set_gas_heater_temperature(320)
+    #sensor.set_gas_heater_duration(150)
+    #sensor.select_gas_heater_profile(0)
     logger.info("Initial sensor config complete")
 
     while True:
