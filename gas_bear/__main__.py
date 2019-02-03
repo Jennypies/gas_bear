243818726e04cfe686ed45052fa9e1e054e9a2ac
Jenny <jenny.thorne3006@gmail.com>
2019-02-03 16:58:44 +0000

    pushy

--- gas_bear/__main__.py
+++ gas_bear/__main__.py
@@ -9,7 +9,10 @@ logger = logging.getLogger(__name__)
 def main():
     logging.basicConfig(format="[%(levelname)s %(name)s] %(message)s", level=logging.INFO)
 
-    sensor = bme680.BME680()
+    try:
+        sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+    except IOError:
+        sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
 
     sensor.set_humidity_oversample(bme680.OS_2X)
     sensor.set_pressure_oversample(bme680.OS_4X)
