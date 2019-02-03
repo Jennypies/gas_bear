51f58e564bc5f64c82b5b42cdd3dcdf7fd4ffd55
Jenny <jenny.thorne3006@gmail.com>
2019-02-03 17:16:15 +0000

    pushy

--- gas_bear/__main__.py
+++ gas_bear/__main__.py
@@ -9,20 +9,17 @@ logger = logging.getLogger(__name__)
 def main():
     logging.basicConfig(format="[%(levelname)s %(name)s] %(message)s", level=logging.INFO)
 
-    try:
-        sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
-    except IOError:
-        sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+    sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
 
     sensor.set_humidity_oversample(bme680.OS_2X)
     sensor.set_pressure_oversample(bme680.OS_4X)
     sensor.set_temperature_oversample(bme680.OS_8X)
     sensor.set_filter(bme680.FILTER_SIZE_3)
 
-    #sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
-    #sensor.set_gas_heater_temperature(320)
-    #sensor.set_gas_heater_duration(150)
-    #sensor.select_gas_heater_profile(0)
+    sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+    sensor.set_gas_heater_temperature(320)
+    sensor.set_gas_heater_duration(150)
+    sensor.select_gas_heater_profile(0)
     logger.info("Initial sensor config complete")
 
     while True:
@@ -33,17 +30,16 @@ def main():
                 sensor.data.temperature,
                 sensor.data.pressure,
                 sensor.data.humidity)
-            print(output)
 
-            #if sensor.data.heat_stable:
-            #    logger.info("Gas sensor data collected")
-            #    print('{0},{1} Ohms'.format(
-            #        output,
-            #        sensor.data.gas_resistance))
+            if sensor.data.heat_stable:
+                logger.info("Gas sensor data collected")
+                logger.info('{0},{1} Ohms'.format(
+                    output,
+                    sensor.data.gas_resistance))
 
-            #else:
-            #    logger.warning("Gas sensor data not heat stable")
-            #    print(output)
+            else:
+                logger.warning("Gas sensor data not heat stable")
+                logger.info(output)
         else:
             logger.warning("No data from sensors")
 
